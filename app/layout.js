d8aa01db7fb1b1e5c055bfe10df3acfd0edd213b
Manas Awasthi <manasawasthi@Manass-MacBook-Air.local>
2026-03-26 13:01:45 +0530

    rebrand: BookBlitz to MiReads

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import Navbar from '@/components/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'BookBlitz - Books Delivered in 10 Minutes',
+  title: 'mireads - Books Delivered in 10 Minutes',
   description: 'Quick commerce books delivery platform',
 };
 
